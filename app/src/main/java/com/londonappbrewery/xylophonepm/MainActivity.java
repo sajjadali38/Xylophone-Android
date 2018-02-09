812a1111ee03ea6500016f919088338ac3afd63f
Sajjad <sajjadali38>
2018-02-09 17:49:13 +0100

    Music_play app

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private final float NORMAL_PLAY_RATE = 1.0f;
 
     // TODO: Add member variables here
+    private SoundPool mSoundPool;
     private int mCSoundId;
     private int mDSoundId;
     private int mESoundId;
@@ -31,15 +33,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
-
+        mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS,AudioManager.STREAM_MUSIC, 0);
 
         // TODO: Load and get the IDs to identify the sounds
-
-
+        mCSoundId = mSoundPool.load(getApplicationContext(),R.raw.note1_c, 1);
+        mCSoundId = mSoundPool.load(getApplicationContext(),R.raw.note2_d, 1);
+        mCSoundId = mSoundPool.load(getApplicationContext(),R.raw.note3_e, 1);
+        mCSoundId = mSoundPool.load(getApplicationContext(),R.raw.note4_f, 1);
+        mCSoundId = mSoundPool.load(getApplicationContext(),R.raw.note5_g, 1);
+        mCSoundId = mSoundPool.load(getApplicationContext(),R.raw.note6_a, 1);
+        mCSoundId = mSoundPool.load(getApplicationContext(),R.raw.note7_b, 1);
     }
 
     // TODO: Add the play methods triggered by the buttons
+    public void PlayC(View view){
+        Log.d("Xylophone", "Red Button Clicked!");
+        mSoundPool.play(mCSoundId, LEFT_VOLUME,RIGHT_VOLUME,NO_LOOP,PRIORITY,NORMAL_PLAY_RATE);
+    }
+    public void PlayD(View view){
+        Log.d("Xylophone", "Red Button Clicked!");
+        mSoundPool.play(mCSoundId, LEFT_VOLUME,RIGHT_VOLUME,NO_LOOP,PRIORITY,NORMAL_PLAY_RATE);
 
+    }
+    public void PlayE(View view){
+        Log.d("Xylophone", "Red Button Clicked!");
+        mSoundPool.play(mCSoundId, LEFT_VOLUME,RIGHT_VOLUME,NO_LOOP,PRIORITY,NORMAL_PLAY_RATE);
+
+    }
+    public void PlayF(View view){
+        Log.d("Xylophone", "Red Button Clicked!");
+        mSoundPool.play(mCSoundId, LEFT_VOLUME,RIGHT_VOLUME,NO_LOOP,PRIORITY,NORMAL_PLAY_RATE);
+
+    }
+    public void PlayG(View view){
+        Log.d("Xylophone", "Red Button Clicked!");
+        mSoundPool.play(mCSoundId, LEFT_VOLUME,RIGHT_VOLUME,NO_LOOP,PRIORITY,NORMAL_PLAY_RATE);
+
+    }
+    public void PlayA(View view){
+        Log.d("Xylophone", "Red Button Clicked!");
+        mSoundPool.play(mCSoundId, LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+
+    }
+    public void PlayB(View view){
+        Log.d("Xylophone", "Red Button Clicked!");
+        mSoundPool.play(mCSoundId, LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+
+    }
 
 
 }
